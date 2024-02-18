ece543dc7b593c3237fa9dff10823098bacbbd88
Phanindra Kumar <phanindra.1756.kkr@gmail.com>
2024-02-18 18:08:11 -0500

    Completed Times

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -9,5 +9,22 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
+    let beforeStart = Date.now()
+    let sum = 1,  counter = 0;
+    while (counter <= n ) {
+        sum += counter
+        counter++
+    }
+    console.log(`Sum from 1 to ${n} is : `, sum);
+    let timeLapsed = Date.now() - beforeStart
+    console.log(`Time(in sec) to complete the calculations: `, timeLapsed/1000)
     return 0.01;
 }
+
+
+// TODO: (not a serious one) change the order of '100000' and '100' the results don't make sense
+// sometimes time for 100 is more the time taken for 100000, and this doesn't happen all the time
+calculateTime(100)
+calculateTime(100000)
+calculateTime(1000000000)
+
