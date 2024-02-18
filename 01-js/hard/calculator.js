edf66ff12fe5b9364a2a8c04f971f6799604be19
Phanindra Kumar <phanindra.1756.kkr@gmail.com>
2024-02-18 18:39:12 -0500

    Completed Calculator

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -14,8 +14,77 @@
         2. the input can have invalid non-numerical characters like `5 + abc`, you're supposed to throw error for such inputs
 
   Once you've implemented the logic, test your code by running
+
+  NOTES: 
+  1. Make sure the logic is capable to handle errors in input like division by zero
+  2. Handle calculations with decimal numbers
 */
 
-class Calculator {}
+class Calculator {
+  constructor() {
+    this.result = 0;
+  }
 
+  add(num) {
+    this.result += num;
+  }
+
+  subtract(num) {
+    this.result -= num;
+  }
+
+  multiply(num) {
+    this.result *= num;
+  }
+
+  divide(num) {
+    if (num === 0) {
+      throw new Error('Division by zero');
+    }
+    this.result = this.result / num;
+  }
+
+  clear() {
+    this.result = 0;
+  }
+
+  getResult() {
+    return this.result;
+  }
+
+  calculate(expression) {
+    // remove all spaces
+    expression = expression.replace(/\s/g, '');
+    // check for invalid characters \d for digits, \. for decimal, \+ for addition, \- for subtraction, \* for multiplication, \/ for division, \( for opening bracket, \) for closing bracket
+    if (/[^\d\.?\+\-\*\/\(\)]/.test(expression)) {
+      throw new Error('Invalid expression');
+    }
+    // check for division by zero
+    if (expression.includes('/0')) {
+      throw new Error('Division by zero');
+    }
+    // evaluate the expression
+    this.result
+      = eval
+        (expression
+          .replace(/\(/g, '(')
+          .replace(/\)/g, ')')
+          .replace(/\+/g, '+')
+          .replace(/\-/g, '-')
+          .replace(/\*/g, '*')
+          .replace(/\//g, '/')
+        );
+
+  }
+
+}
+
+// test your code
+const calculator = new Calculator();
+calculator.add(10);
+calculator.subtract(5);
+calculator.multiply(2);
+calculator.divide(2);
+console.log(calculator.getResult()); // 5
+calculator.clear();
 module.exports = Calculator;
