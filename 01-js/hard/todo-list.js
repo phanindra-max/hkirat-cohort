d5204dd21a4f9e8d7a06c7c9a71c8e6ec6be7599
Phanindra Kumar <phanindra.1756.kkr@gmail.com>
2024-02-18 19:06:23 -0500

    Completed To-Dos

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,7 +11,40 @@
 */
 
 class Todo {
-
+  constructor() {
+    this.list = []
+  }
+  add(todo) {
+    this.list.push(todo)
+  }
+  remove(indexOfTodo) {
+    try {
+      this.list.splice(indexOfTodo, 1)
+    } catch (e) {
+      throw new Error('Invalid index')
+    }
+  }
+  update(index, updatedTodo){
+    try {
+      if (this.list.length > index) {this.list.splice(index, 1, updatedTodo)}
+    } catch (e) {
+      throw new Error('Invalid index')
+    }
+  }
+  getAll(){
+    return this.list
+  }
+  get (indexOfTodo) {
+    try {
+      if (this.list.length > indexOfTodo) {return this.list[indexOfTodo]}
+      return null
+    } catch (e) {
+      throw new Error('Invalid index')
+    }
+  }
+  clear () {
+    this.list = []
+  }
 }
 
 module.exports = Todo;
