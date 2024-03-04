cea3ccb746ca0ac5c8f436b915e09bffadfef05d
Phanindra Kumar <phanindra.1756.kkr@gmail.com>
2024-03-04 12:35:04 -0500

    Completed Week-2 /2-sleep-completely busy wait

--- week-2/01-async-js/hard (promises)/2-sleep-completely.js
+++ week-2/01-async-js/hard (promises)/2-sleep-completely.js
@@ -5,6 +5,13 @@
  */
 
 function sleep(milliseconds) {
+  return new Promise((resolve, reject) => {
+    let start = Date.now();
+    while (Date.now() - start < milliseconds) {
+      // do nothing
+    }
+    resolve();
+  });
 }
 
 module.exports = sleep;
